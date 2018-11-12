c52cb255f159fad5bb28307839c5ba3f539003d3
Emil Johannessen <emil.jona@gmai.com>
2018-11-12 19:08:12 +0100

    Convertet tetris.js to Vue-component

--- src/components/tetris/Tetris.vue
+++ src/components/tetris/Tetris.vue
@@ -6,8 +6,56 @@
 </template>
 
 <script>
+import { gameBoard } from './tetris'
+
 export default {
-  name: 'tetris'
+  name: 'tetris',
+  data: function() {
+    return {
+      gameBoard,
+      currentI: 0,
+      currentcolumn: 4
+    }
+  },
+  methods: {
+    newBlock: function() {
+      this.gameBoard[this.currentcolumn][0] = 1
+    },
+    goLeft: function() {
+      if (this.currentcolumn === 0) return
+      if (this.gameBoard[this.currentcolumn - 1][this.currentI] === 2) return
+      this.gameBoard[this.currentcolumn][this.currentI] = 0
+      this.gameBoard[this.currentcolumn - 1][this.currentI] = 1
+      this.currentcolumn--
+    },
+    goRight: function() {
+      if (this.currentcolumn === 9) return
+      if (this.gameBoard[this.currentcolumn + 1][this.currentI] === 2) return
+      this.gameBoard[this.currentcolumn][this.currentI] = 0
+      this.gameBoard[this.currentcolumn + 1][this.currentI] = 1
+      this.currentcolumn++
+    },
+    move: function() {
+      switch (this.gameBoard[this.currentcolumn][this.currentI + 1]) {
+        case undefined:
+          this.gameBoard[this.currentcolumn][this.currentI] = 2
+          this.currentI = 0
+          newBlock()
+          break
+        case 2:
+          if (currentI === 0) return
+          //Do something here....
+          this.gameBoard[this.currentcolumn][this.currentI] = 2
+          this.currentI = 0
+          newBlock()
+          break
+        default:
+          this.gameBoard[this.currentcolumn][this.currentI] = 0
+          this.gameBoard[this.currentcolumn][this.currentI + 1] = 1
+          this.currentI++
+      }
+    }
+  }
 }
 </script>
 
--- src/components/tetris/tetris.js
+++ src/components/tetris/tetris.js
@@ -1,4 +1,4 @@
-const row = [
+export const gameBoard = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -15,45 +15,45 @@ let currentcolumn = 4
 
 function print() {
   console.log('******')
-  console.log(row.join('\n'))
+  console.log(gameBoard.join('\n'))
 }
 
 function newBlock() {
-  row[currentcolumn][0] = 1
+  gameBoard[currentcolumn][0] = 1
 }
 
 function goLeft() {
   if (currentcolumn === 0) return
-  if (row[currentcolumn - 1][currentI] === 2) return
-  row[currentcolumn][currentI] = 0
-  row[currentcolumn - 1][currentI] = 1
+  if (gameBoard[currentcolumn - 1][currentI] === 2) return
+  gameBoard[currentcolumn][currentI] = 0
+  gameBoard[currentcolumn - 1][currentI] = 1
   currentcolumn--
 }
 
 function goRight() {
   if (currentcolumn === 9) return
-  if (row[currentcolumn + 1][currentI] === 2) return
-  row[currentcolumn][currentI] = 0
-  row[currentcolumn + 1][currentI] = 1
+  if (gameBoard[currentcolumn + 1][currentI] === 2) return
+  gameBoard[currentcolumn][currentI] = 0
+  gameBoard[currentcolumn + 1][currentI] = 1
   currentcolumn++
 }
 
 function move() {
-  switch (row[currentcolumn][currentI + 1]) {
+  switch (gameBoard[currentcolumn][currentI + 1]) {
     case undefined:
-      row[currentcolumn][currentI] = 2
+      gameBoard[currentcolumn][currentI] = 2
       currentI = 0
       newBlock()
       break
     case 2:
-      if (currentI === 0) throw Error('Game OVer')
-      row[currentcolumn][currentI] = 2
+      if (currentI === 0) thgameBoard Error('Game OVer')
+      gameBoard[currentcolumn][currentI] = 2
       currentI = 0
       newBlock()
       break
     default:
-      row[currentcolumn][currentI] = 0
-      row[currentcolumn][currentI + 1] = 1
+      gameBoard[currentcolumn][currentI] = 0
+      gameBoard[currentcolumn][currentI + 1] = 1
       currentI++
   }
 }
