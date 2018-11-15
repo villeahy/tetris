dd266423fdd965873909377894f72f02f8bd4d41
Emil Johannessen <emil.jona@gmai.com>
2018-11-15 14:19:27 +0100

    Added styles for the front-page

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,22 @@ export default {
 </script>
 
 <style>
+@import 'assets/css/normalize.css';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+ul {
+  list-style: none;
+  padding-left: 0;
 }
 </style>
--- src/components/pages/MainPage.vue
+++ src/components/pages/MainPage.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>
+  <div id="MainPage">
     <div class="header">
-      <h1>Welcome to "insert tetris game name here"</h1>
+      <h1>Welcome to Tetris!</h1>
       <p>This game is a part of our project in the project course at Howest.
       We are a group of three international students from Poland, Finland and Norway.</p>
     </div>
     <nav>
       <ul>
-        <li><router-link to='/tetris'>Tetris</router-link></li>
+        <router-link to='/tetris'><li class="play-btn">Play game</li></router-link>
       </ul>
     </nav>
   </div>
@@ -21,8 +21,46 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#MainPage {
+  background: url('../../assets/img/background-front-page.jpg') center;
+  height: 100vh;
+  padding: 1px;
+}
 .header {
-  background: lightblue;
-  padding: 20px;
+  color: #4b6c77;
+  margin-top: calc(50vh - 109.4px);
+  padding: 10px;
+  max-width: 500px;
+  display: inline-block;
+  margin: calc(50vh - 109.4px) 0 auto;
+}
+
+.play-btn {
+  background-color: lightblue;
+  display: inline-block;
+  padding: 10px 20px;
+  font-size: 1.5em;
+  text-transform: capitalize;
+  font-weight: bold;
+  color: white;
+  border-radius: 5px;
+}
+
+.play-btn a {
+  text-decoration: none;
+}
+
+.play-btn:hover {
+  color: #00d624;
+  animation: toBig 0.1s ease-out forwards;
+}
+
+@keyframes toBig {
+  from {
+    transform: scale(1);
+  }
+  to {
+    transform: scale(1.2);
+  }
 }
 </style>
