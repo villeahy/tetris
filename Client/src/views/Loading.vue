b6db2b01335c1ae78a19543d51e340219c6066ef
Emil Johannessen <emil.jona@gmail.com>
2018-12-15 21:35:43 +0100

    Some smaller adjustments to loading style

--- Client/src/views/Loading.vue
+++ Client/src/views/Loading.vue
@@ -1,9 +1,8 @@
 <template lang="html">
-    <div class="waiting">
-    <p>Waiting for an opponent to join...</p>
-    <p>(Try open a new tab/browser)</p>
-    </div>
-
+  <div class="waiting">
+  <p class="waiting-text">Waiting for an opponent to join...</p>
+  <p class="waiting-hint">(Try open a new tab/browser)</p>
+  </div>
 </template>
 
 <script>
@@ -20,4 +19,19 @@ export default {}
     color: #009f00;
     font-weight: 600;
   }
+
+  .waiting-text {
+    animation: fadeInOut 1s infinite;
+  }
+
+  .waiting-hint {
+    font-weight: lighter;
+    font-size: 10px;
+  }
+
+  @keyframes fadeInOut {
+  0%   { opacity:1; }
+  50%  { opacity:0; }
+  100% { opacity:1; }
+}
 </style>
