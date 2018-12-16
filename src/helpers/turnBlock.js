3c37bbe484a9ae6013f1182e8b6bb6a576b9e2e1
Emil Johannessen <emil.jona@gmail.com>
2018-12-16 02:03:20 +0100

    added turn for case 6

--- src/helpers/turnBlock.js
+++ src/helpers/turnBlock.js
@@ -29,6 +29,28 @@ export function turnBlock({ value, coords, turn }) {
         coords: [first, second, third, fourth],
         turn: (turn + 1) % 2
       };
+      case 6:
+        console.log("turn block case 6");
+        if (turn) {
+          second.row = second.row + 1;
+          second.column = second.column - 1;
+          third.row = third.row + 2;
+          third.column = third.column -2;
+          fourth.row = fourth.row + 3;
+          fourth.column = fourth.column -3;
+        } else {
+          second.row = second.row - 1;
+          second.column = second.column + 1;
+          third.row = third.row - 2;
+          third.column = third.column + 2;
+          fourth.row = fourth.row - 3;
+          fourth.column = fourth.column + 3;
+        }
+        return {
+          value,
+          coords: [first, second, third, fourth],
+          turn: (turn + 1) % 2
+        };
     default:
       console.log("turn block default");
       return { value, coords, turn };
