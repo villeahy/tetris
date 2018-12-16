57682e90e73c12e73b4141c299967443d19ebd57
ville.hytonen <ville.hytonen@student.howest.be>
2018-12-16 02:19:12 +0100

    block with value 3 made turning

--- src/Classes/Game.js
+++ src/Classes/Game.js
@@ -95,7 +95,7 @@ export default class {
   // Tetris logic//////////////////////////////////Math.floor(Math.random() * 7)
 
   newBlock() {
-    this.block = blockGenerator(Math.floor(Math.random() * 7));
+    this.block = blockGenerator(2);
   }
 
   setValues() {
